af0fdbdae6784a7db88b047ac7ba4b51848bf8cf
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-03-12 01:51:56 -0300

    Centrado de imagenes de proyectos.

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -7,9 +7,9 @@ export default function ProjectCard({ title, description, tecnologies, imgUrl, g
     <div>
         <div 
             className='h-52 md:h-72 rounded-t-xl relative group' 
-            style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}>   
+            style={{ background: `url(${imgUrl})`, backgroundSize: "contain", backgroundPositionY: "top", backgroundRepeat: "no-repeat" }}>   
         
-            <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
+            <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:rounded-t-xl group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
                 {gitUrl !== ''
                     ? <Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
                         <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white'/>
@@ -24,7 +24,7 @@ export default function ProjectCard({ title, description, tecnologies, imgUrl, g
                 }
             </div>
         </div>
-        <div className='text-white rounded-b-xl mt-2 bg-[#181818] py-6 px-4'>
+        <div className='text-white rounded-b-xl mt-1 bg-[#181818] py-6 px-4'>
             <h5 className='font-xl font-semibold mb-2'>{title}</h5>
             <p className='text-[#ADB7BE]'>{description}</p>
             <h6 className='font-xl font-semibold mb-2 mt-4'>Tecnologías:</h6>
