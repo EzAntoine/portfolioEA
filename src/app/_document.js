5c591aa360330975693e677fe58eaa601bc0babe
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-03-12 00:46:54 -0300

    Agregue _document.js para agregar icono a pestaña

---
+++ src/app/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Agrega el enlace al icono de la pestaña del navegador */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
