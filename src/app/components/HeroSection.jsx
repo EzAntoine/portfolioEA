a9b5dfcc75980c396bf926b97cb8a3acf8b2a298
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-04-12 13:49:48 -0300

    Se corrigio url de git y linkedin en mobile, se agrego nombre de tecnologias, se acomodo separacion foto con descripcion de proyectos

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client";
-import React from 'react'
-import { TypeAnimation } from 'react-type-animation'
-import Link from 'next/link';
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
 import GithubIcon from "../../../public/github.svg";
 import LinkedinIcon from "../../../public/linkedin.svg";
 import MailIcon from "../../../public/mail.svg";
@@ -10,58 +10,84 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <section>
-        <div className='grid grid-cols-1 lg:grid-cols-12'>
-            <div className='col-span-9 place-self-center text-center sm:text-left'>
-                <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-5xl lg:leading-normal font-extrabold">
-                    <span className='text-transparent bg-clip-text bg-gradient-to-br from-green-500 via-lime-500 to-yellow-500'>
-                         Hola! Soy{" "}
-                    </span>
-                    <br></br>
-                    <TypeAnimation
-                        sequence={[
-                            'Ezequiel Antoine',
-                            1000, 
-                            'Desarrollador FullStack',
-                            1000
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={1}
-                    />
-                </h1>
-                <p className='text-[#6c7175] text-base sm:text-lg mb-6 lg:text-xl'>
-                    Una persona versátil y apasionada, con experiencia laboral en diversos rubros, explorando diferentes industrias y roles, lo que me ha permitido desarrollar perspectiva y habilidad para adaptarme rápidamente a nuevos desafíos, siempre desde el compromiso y la empatía. 
-                    Como desarrollador, combino mis hobbies y vivencias en otros campos con mi capacidad para pensar fuera de la caja para crear soluciones con la idea de ser lo mas simples y efectivas posibles. 
-                </p>
-                <Link 
-                    href="/CV_Ezequiel_Antoine.pdf" download
-                    target="_blank"
-                    className='flex flex-wrap justify-between'>
-                    <button className='px-1 py-1 w-full sm:w-fit rounded-sm mr-4 bg-transparent font-semibold bg-gradient-to-br from-green-500 via-lime-500 to-yellow-500 hover:bg-slate-800 text-white border border-white mt-3'>
-                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-sm px-5 py-1'>
-                            Descargar CV
-                        </span>
-                    </button>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-9 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-5xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-green-500 via-lime-500 to-yellow-500">
+              Hola! Soy{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Ezequiel Antoine",
+                1000,
+                "Desarrollador FullStack",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={1}
+            />
+          </h1>
+          <p className="text-[#6c7175] text-base sm:text-lg mb-6 lg:text-xl">
+            Una persona versátil y apasionada, con experiencia laboral en
+            diversos rubros, explorando diferentes industrias y roles, lo que me
+            ha permitido desarrollar perspectiva y habilidad para adaptarme
+            rápidamente a nuevos desafíos, siempre desde el compromiso y la
+            empatía. Como desarrollador, combino mis hobbies y vivencias en
+            otros campos con mi capacidad para pensar fuera de la caja para
+            crear soluciones con la idea de ser lo mas simples y efectivas
+            posibles.
+          </p>
+          <Link
+            href="/CV_Ezequiel_Antoine.pdf"
+            download
+            target="_blank"
+            className="flex flex-wrap justify-between"
+          >
+            <button className="px-1 py-1 w-full sm:w-fit rounded-sm mr-4 bg-transparent font-semibold bg-gradient-to-br from-green-500 via-lime-500 to-yellow-500 hover:bg-slate-800 text-white border border-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-sm px-5 py-1">
+                Descargar CV
+              </span>
+            </button>
+          </Link>
+          <div className="container md:hidden p-1 flex flex-wrap items-center">
+            <div className="flex flex-wrap gap-2 justify-center w-full">
+              <div className="flex flex-row ">
+                <Link href="https://github.com/EzAntoine" target="_blank">
+                  <Image
+                    src={GithubIcon}
+                    alt="Github"
+                    title="github.com/EzAntoine"
+                    className="p-0.5 w-10 h-10 rounded-sm bg-white mr-3 transform hover:scale-110 transition-transform duration-300 ease-in-out"
+                  />
                 </Link>
-                <div className="container md:hidden p-1 flex flex-wrap items-center">         
-                    <div className="flex flex-wrap gap-2 justify-center w-full">
-                        <div className='flex flex-row '>
-                            <Link href="github.com/EzAntoine">
-                                <Image src={GithubIcon} alt="Github" title='github.com/EzAntoine' className='p-0.5 w-10 h-10 rounded-sm bg-white mr-3 transform hover:scale-110 transition-transform duration-300 ease-in-out'/>
-                            </Link>
-                            <Link href="linkedin.com/in/ezequielantoine">
-                                <Image src={LinkedinIcon} alt="LinkedIn" title='linkedin.com/in/ezequielantoine' className='p-0.5 w-10 h-10 rounded-sm bg-white mr-1 transform hover:scale-110 transition-transform duration-300 ease-in-out'/>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link href="mailto:ezequielantoine@gmail.com">
-                                <Image src={MailIcon} alt="Email" title='ezequielantoine@gmail.com' className='p-0.5 w-10 h-10 rounded-sm bg-white transform hover:scale-110 transition-transform duration-300 ease-in-out'/>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>   
+                <Link
+                  href="https://linkedin.com/in/ezequielantoine"
+                  target="_blank"
+                >
+                  <Image
+                    src={LinkedinIcon}
+                    alt="LinkedIn"
+                    title="linkedin.com/in/ezequielantoine"
+                    className="p-0.5 w-10 h-10 rounded-sm bg-white mr-1 transform hover:scale-110 transition-transform duration-300 ease-in-out"
+                  />
+                </Link>
+              </div>
+              <div>
+                <Link href="mailto:ezequielantoine@gmail.com">
+                  <Image
+                    src={MailIcon}
+                    alt="Email"
+                    title="ezequielantoine@gmail.com"
+                    className="p-0.5 w-10 h-10 rounded-sm bg-white transform hover:scale-110 transition-transform duration-300 ease-in-out"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
+import React from "react";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-export default function ProjectCard({ title, description, tecnologies, imgUrl, gitUrl, siteUrl }) {
+export default function ProjectCard({
+  title,
+  description,
+  tecnologies,
+  imgUrl,
+  gitUrl,
+  siteUrl,
+}) {
   return (
     <div>
-        <div 
-            className='h-52 md:h-72 rounded-t-xl relative group' 
-            style={{ background: `url(${imgUrl})`, backgroundSize: "contain", backgroundPositionY: "top", backgroundRepeat: "no-repeat" }}>   
-        
-            <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:rounded-t-xl group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
-                {gitUrl !== ''
-                    ? <Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
-                        <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white'/>
-                    </Link>
-                    : null
-                }
-                {siteUrl !== '' 
-                    ? <Link href={siteUrl} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
-                        <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white'/>
-                    </Link>
-                    : null
-                }
-            </div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{
+          background: `url(${imgUrl})`,
+          backgroundSize: "cover",
+          backgroundPositionY: "top",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:rounded-t-xl group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          {gitUrl !== "" ? (
+            <Link
+              href={gitUrl}
+              className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            >
+              <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+            </Link>
+          ) : null}
+          {siteUrl !== "" ? (
+            <Link
+              href={siteUrl}
+              className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            >
+              <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+            </Link>
+          ) : null}
         </div>
-        <div className='text-white rounded-b-xl mt-1 bg-[#181818] py-6 px-4'>
-            <h5 className='font-xl font-semibold mb-2'>{title}</h5>
-            <p className='text-[#ADB7BE]'>{description}</p>
-            <h6 className='font-xl font-semibold mb-2 mt-4'>Tecnologías:</h6>
-            <p className='text-[#ADB7BE] text-sm'>{tecnologies}</p>
-        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-1 bg-[#181818] py-6 px-4">
+        <h5 className="font-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+        <h6 className="font-xl font-semibold mb-2 mt-4">Tecnologías:</h6>
+        <p className="text-[#ADB7BE] text-sm">{tecnologies}</p>
+      </div>
     </div>
-  )
+  );
 }
