a3515ee3c465ae7d7e6e6a1d90b8b6419326dd10
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-25 11:45:42 -0300

    Instalacion de dependencias - cambio de NODE_ENV

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
@@ -7,15 +6,15 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black">
-      <div className='container mx-auto px-12 py-4'>
-        <Navbar/>
-        <div className="container mt-24 mx-auto">
-          <HeroSection/>
-        </div>  
-        <About/>
-        <ProjectsSection/>
-        <Footer/>
+    <main className="flex flex-col min-h-screen bg-black">
+      <div className="container px-12 py-4 mx-auto">
+        <Navbar />
+        <div className="container mx-auto mt-24">
+          <HeroSection />
+        </div>
+        <About />
+        <ProjectsSection />
+        <Footer />
       </div>
     </main>
   );
