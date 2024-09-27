9f7f10774083ce8af796143f1444bf6a7f035213
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-27 01:31:37 -0300

    Agregue Head con metadata en page

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,24 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import ProjectsSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-black">
+      <Head>
+        <title>Ezequiel Antoine - Portfolio</title>
+        <meta
+          name="description"
+          content="Portfolio personal - Ezequiel Antoine"
+        ></meta>
+        <meta property="og:title" content="Ezequiel Antoine - Portfolio"></meta>
+        <meta
+          property="og:description"
+          content="Portfolio personal - Ezequiel Antoine"
+        ></meta>
+        <meta property="og:image" content="/public/preview.png"></meta>
+      </Head>
       <div className="container px-12 py-4 mx-auto">
         <Navbar />
         <div className="container mx-auto mt-24">
